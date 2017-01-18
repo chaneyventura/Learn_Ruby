5577d6eba80a3ab27ab036ec16116b8bee98ecfb
chaneyventura <chaneyventura@gmail.com>
2017-01-18 16:29:21 -0700

    exercise 12 prompting people fo numbers

---
+++ ex11.rb
@@ -0,0 +1,15 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall, and #{weight} heavy."
+
+print "When is your birthday? "
+birthday = gets.chomp
+print "What is your favorite ice cream? "
+icecream = gets.chomp
+
+puts "Let's eat #{icecream} on #{birthday}!"
